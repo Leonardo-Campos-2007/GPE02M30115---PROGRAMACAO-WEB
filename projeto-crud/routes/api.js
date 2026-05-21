3adc63f1c5536bf3751a0d683048b99bf705adcc
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2026-05-21 12:53:03 -0300

    Create api.js

---
+++ projeto-crud/routes/api.js
@@ -0,0 +1,13 @@
+const express = require('express'); //instancia do express
+
+const router = express.Router(); //modulariza as rotas
+
+const db = require('../db'); //conecta com o banco de dados
+
+//criar as rotas
+//cadastrar usuário
+//editar usuário
+//listar todos os usuários
+//excluir o usuário
+
+module.exports = router;
