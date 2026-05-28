cdf2e87591d6e36ffda5fd568bc2b659084cb2ea
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2026-05-28 11:58:36 -0300

    add

--- projeto-crud/db.js
+++ projeto-crud/db.js
@@ -4,16 +4,18 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'admin123', //catolica
-    database: 'userdb_ranyelson2', //nome do banco de dados
-    port: '3306' //3307
+    password: 'catolica', //catolica
+    database: 'userdb_leonardo', //nome do banco de dados
+    port: '3307' //3307
 });
 
 //estabelecer a conexão
 db.connect(err =>{
     if(err) throw err;
     console.log('conectado ao banco de dados');
+    //localhost:3307/userdb_leonardo
 });
 
 //exportar o módulo de conexão
 module.exports = db;
+
